dd7b80ba1e5c769744261c9559d7ceacfec990ec
TheUnknown550 <mattcosh06@gmail.com>
2025-10-31 14:57:23 +0700

    Add gitignore Started the mean_vaar_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,7 +1,11 @@
 import numpy as np
 
 def calculate(list):
-
+    if len(list) != 9: # the list must be 9
+        raise ValueError("List must contain nine numbers.")
+    
+    # Build dictionary with all required calculations
+    calculations = {}
 
 
 
